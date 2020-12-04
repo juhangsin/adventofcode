8bee131acecbcff7ff63a76b5f00007f3f5d8ce4
JuHang Sin <juhangsin@gmail.com>
2020-12-04 00:44:01 -0500

    part 2

--- day3/3rd.js
+++ day3/3rd.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const input = fs.readFileSync('./input.txt', 'utf-8').split('\n')
 
-let tree = 0;
 let currentX = 0;
 let currentY = 0;
 const totalLength = input[0].length;
 
 function slide(moveRight, moveDown) {
     
+    let tree = 0;
     currentX = moveRight;
     currentY = moveDown;
     
@@ -18,8 +18,20 @@ function slide(moveRight, moveDown) {
         currentY += moveDown;
         currentX = (currentX + moveRight) % totalLength;
     } while (currentY < input.length);
-    console.log(tree);
+    console.log(tree)
     return tree;
 }
 
-slide(3, 1)
+let total = 1;
+const test = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2]
+]
+
+test.map(location => {
+    total *= slide(...location)
+})
+console.log(total)
